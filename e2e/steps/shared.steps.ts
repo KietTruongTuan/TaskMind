335333095f9761da041d97cdcf74eb2311209db4
Kent <leanhkhoibk@gmail.com>
2025-08-03 20:28:47 +0700

    e2e and report

--- e2e/steps/shared.steps.ts
+++ e2e/steps/shared.steps.ts
@@ -1,17 +1,21 @@
-import { Given, Then } from '@cucumber/cucumber';
-import { Browser, chromium, expect, Page } from '@playwright/test';
+import { Given, Then, After } from "@cucumber/cucumber";
+import { Browser, chromium, expect, Page } from "@playwright/test";
 
-let browser: Browser
-let page: Page
+let browser: Browser;
+let page: Page;
 
-Given('I am on the homepage', async function () {
-    // Write code here that turns the phrase above into concrete actions
-    browser = await chromium.launch()
-    page = await browser.newPage()
-    await page.goto("https://localhost:3000/")
+Given("I am on the homepage", async function () {
+  // Write code here that turns the phrase above into concrete actions
+  browser = await chromium.launch();
+  page = await browser.newPage();
+  await page.goto("http://localhost:3000/");
 });
 
-Then('I should see {string}', function (text: string) {
-    // Write code here that turns the phrase above into concrete actions
-    expect(page.getByText(text)).toBeVisible()
+Then("I should see {string}", async function (text: string) {
+  // Write code here that turns the phrase above into concrete actions
+  await expect(page.getByText(text)).toBeVisible();
+});
+
+After(async function () {
+  await browser?.close(); // closes Chromium and ends the process
 });
