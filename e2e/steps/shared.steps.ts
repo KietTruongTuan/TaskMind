e6e4ad00ea3f82130ed6f1f26b55e1a634cf0670
Kent <leanhkhoibk@gmail.com>
2025-08-09 21:58:46 +0700

    report screen shot and eslint

--- e2e/steps/shared.steps.ts
+++ e2e/steps/shared.steps.ts
@@ -1,21 +1,49 @@
-import { Given, Then, After } from "@cucumber/cucumber";
-import { Browser, chromium, expect, Page } from "@playwright/test";
+import {
+  Given,
+  Then,
+  setWorldConstructor,
+  World,
+  IWorldOptions,
+  When,
+} from "@cucumber/cucumber";
+import { expect, Browser, Page, chromium } from "@playwright/test";
 
-let browser: Browser;
-let page: Page;
+export class CustomWorld extends World {
+  browser!: Browser;
+  page!: Page;
 
-Given("I am on the homepage", async function () {
-  // Write code here that turns the phrase above into concrete actions
-  browser = await chromium.launch();
-  page = await browser.newPage();
-  await page.goto("http://localhost:3000/");
+  constructor(options: IWorldOptions) {
+    super(options);
+  }
+
+  async init(): Promise<void> {
+    this.browser = await chromium.launch();
+    const context = await this.browser.newContext();
+    this.page = await context.newPage();
+  }
+
+  async close(): Promise<void> {
+    await this.browser.close();
+  }
+}
+
+setWorldConstructor(CustomWorld);
+
+Given("I am on the homepage", async function (this: CustomWorld) {
+  await this.page.goto("http://localhost:3000/");
 });
 
-Then("I should see {string}", async function (text: string) {
-  // Write code here that turns the phrase above into concrete actions
-  await expect(page.getByText(text)).toBeVisible();
+Then("I should see {string}", async function (this: CustomWorld, text: string) {
+  await expect(this.page.getByText(text)).toBeVisible();
 });
 
-After(async function () {
-  await browser?.close(); // closes Chromium and ends the process
+When(
+  "I clicks the button {string}",
+  async function (this: CustomWorld, buttonText: string) {
+    await this.page.getByText(buttonText).click();
+  }
+);
+
+Then("I should see the url {string}", async function (url: string) {
+  await expect(this.page).toHaveURL(url);
 });
