f9ad32847a4fdcc9af2f4d0226e5114377dc4035
KoderuNoKo <4trung9kien123@gmail.com>
2026-03-17 18:47:00 +0700

    fix: fix live code not working

--- FE/web-ui/next.config.ts
+++ FE/web-ui/next.config.ts
@@ -2,9 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
     return config;
   },
-  turbopack: {},
 };
 
 export default nextConfig;
