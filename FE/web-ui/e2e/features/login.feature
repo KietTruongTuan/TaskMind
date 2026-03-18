36f0c3f034fc1ed4effd8952a562d2dca8bd0868
KoderuNoKo <4trung9kien123@gmail.com>
2026-03-18 22:51:45 +0700

    test: refactor login test

--- FE/web-ui/e2e/features/login.feature
+++ FE/web-ui/e2e/features/login.feature
@@ -5,13 +5,14 @@ Feature: User login
 
     Scenario: Successful login with valid credentials
         Given I am on the login page
-        When I enter "kien.le4941010@hcmut.edu.vn" and "Kien@494"
-        And I click the login button
+        When I enter "example@gmail.com" into the "Email" field
+        And I enter "ExamplePassword123" into the "Password" field
+        And I click the "Sign In" button
         Then I should see the "Dashboard" page
-        And I should see the greeting Hi, "Kien"! 
-        And I should see the "Recent Goals" section
-        And I should see the metrics:
+        And I should see the headline "Hi, TestUser!" 
+        And I should see the followings:
+            | Recent Goals|
             | Total Goal  |
             | Completed   |
             | In Progress |
-            | Overdue     |
+            | Ovrdue     |
--- FE/web-ui/e2e/steps/login.steps.ts
+++ FE/web-ui/e2e/steps/login.steps.ts
@@ -1,34 +1,6 @@
-import { Given, When, Then, DataTable } from '@cucumber/cucumber';
-import { expect } from '@playwright/test';
+import { Given } from '@cucumber/cucumber';
 import { CustomWorld } from './shared.steps';
 
 Given('I am on the login page', async function (this: CustomWorld) {
     await this.page.goto('http://localhost:3000/tm/authentication');
-});
-
-When('I enter {string} and {string}', async function (this: CustomWorld, email: string, password: string) {
-    await this.page.getByLabel('Email').fill(email);
-    await this.page.getByLabel("Password").fill(password);
-});
-
-When('I click the login button', async function (this: CustomWorld) {
-    await this.page.getByRole('button', { name: 'Sign In', exact: true }).click();
-});
-
-Then('I should see the {string} page', async function (this: CustomWorld, expectedText: string) {
-    await expect(this.page.getByText(expectedText).first()).toBeVisible();
-});
-Then('I should see the greeting Hi, {string}!', async function (this: CustomWorld, username: string) {
-    await expect(this.page.getByRole('heading', {name: `Hi, ${username}!`}).first()).toBeVisible();
-});
-
-Then('I should see the {string} section', async function (this: CustomWorld, sectionName: string) {
-    await expect(this.page.getByText(sectionName).first()).toBeVisible();
-});
-
-Then('I should see the metrics:', async function (this: CustomWorld, dataTable: DataTable) {
-    const metrics = dataTable.raw().flat();
-    for (const metric of metrics) {
-        await expect(this.page.getByText(metric).first()).toBeVisible();
-    }
 });
