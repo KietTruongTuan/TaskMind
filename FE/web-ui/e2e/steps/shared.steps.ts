11a2517905f244839ab6ae1ec59344827c27256f
KoderuNoKo <4trung9kien123@gmail.com>
2026-03-22 20:16:34 +0700

    feat: test with saved cookies, minimal test for creating goal

--- FE/web-ui/e2e/steps/shared.steps.ts
+++ FE/web-ui/e2e/steps/shared.steps.ts
@@ -1,38 +1,56 @@
 import {
+  Given,
   Then,
   setWorldConstructor,
   World,
   IWorldOptions,
   When,
-  DataTable
+  DataTable,
 } from "@cucumber/cucumber";
 import { expect, Browser, Page, chromium } from "@playwright/test";
 
-const PAGE_ROUTES: {[key: string]: string} = {
-  'Dashboard': '/dashboard',
-}
+const HOST_DOMAIN = 'http://localhost:3000';
+export const PAGE_ROUTES: {[key: string]: string} = {
+  'Log in'      : `${HOST_DOMAIN}/tm/authentication`,
+  'Dashboard'   : `${HOST_DOMAIN}/tm/workspace/dashboard`,
+  'New Goal'    : `${HOST_DOMAIN}/tm/workspace/goal/add`,
+};
 
 export class CustomWorld extends World {
   browser!: Browser;
-  page!: Page;
+  page!: Page;  
 
   constructor(options: IWorldOptions) {
     super(options);
   }
 
-  async init(): Promise<void> {
+  async init(requiresAuth: boolean = false, authState?: any): Promise<void> {
     this.browser = await chromium.launch();
-    const context = await this.browser.newContext();
+    const contextOptions = (requiresAuth && authState)
+    ? { storageState: authState }
+    : {};
+
+    const context = await this.browser.newContext(contextOptions);
     this.page = await context.newPage();
   }
 
   async close(): Promise<void> {
     await this.browser.close();
   }
-}
-
+};
 
 setWorldConstructor(CustomWorld);
+
+
+
+
+Given('I am on the {string} page', async function (this: CustomWorld, pageName: string) {
+    await this.page.goto(PAGE_ROUTES[pageName]);
+});
+
+
+
+
 
 When('I click the {string} button', async function (this: CustomWorld, buttonName) {
     await this.page.getByRole('button', { name: buttonName, exact: true }).click();
@@ -45,6 +63,10 @@ When('I enter {string} into the {string} field', async function (this: CustomWor
 
 
 
+
+Then('I should see the text {string}', async function (this:CustomWorld, expectedText: string) {
+    await expect(this.page.getByText(expectedText, { exact: true })).toBeVisible();
+});
 
 Then('I should see the headline {string}', async function (this: CustomWorld, username: string) {
     await expect(this.page.getByRole('heading', {name: username}).first()).toBeVisible();
@@ -62,8 +84,7 @@ Then('I should see the {string} page', async function (this: CustomWorld, pageNa
     if (!expectedPath) {
       throw new Error(`Path of page ${pageName} is not defined!`)
     }
-
-    await expect(this.page).toHaveURL(new RegExp(`.*${expectedPath}`));
+    await expect(this.page).toHaveURL(expectedPath);
   
     // expect pages to have test id follows format "page-name-tab"
     const pageTestId = `${pageName.toLowerCase().replace(/\s/g, '-')}`;
@@ -73,3 +94,8 @@ Then('I should see the {string} page', async function (this: CustomWorld, pageNa
     await expect(tabLocator).toBeVisible();
     await expect(tabLocator).toBeDisabled(); 
 });
+
+Then('I should see the {string} button', async function (this:CustomWorld, expectedButton) {
+    const buttonLocator = await this.page.getByRole('button', { name: expectedButton, exact: true});
+    await expect(buttonLocator).toBeVisible();
+})
