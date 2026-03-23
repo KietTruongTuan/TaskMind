a2f046747b902175694edfe928e649fc4dc3fede
KoderuNoKo <4trung9kien123@gmail.com>
2026-03-23 21:32:24 +0700

    test: login with invalid credentials

--- FE/web-ui/e2e/steps/login.steps.ts
+++ FE/web-ui/e2e/steps/login.steps.ts
@@ -1,4 +1,4 @@
-import { Given } from '@cucumber/cucumber';
+import { Given, Then } from '@cucumber/cucumber';
 import { CustomWorld ,PAGE_ROUTES} from './shared.steps';
 import { expect } from '@playwright/test';
 
@@ -9,4 +9,16 @@ Given('I am logged in', async function (this: CustomWorld) {
     
     // wait for the login to complete before next step
     await expect(this.page).toHaveURL(PAGE_ROUTES['Log in'])
+});
+
+
+
+
+
+Then('I should see the error toast {string}', async function (this:CustomWorld, expectedErrMsg: string) {
+    // expect a toast notification
+    const toastLocator = await this.page.getByRole('alert').filter({ hasText: expectedErrMsg });
+    // expect the error message to be in red
+    await expect(toastLocator).toBeVisible();
+    await expect(toastLocator).toHaveClass(/.*red*/)
 })
