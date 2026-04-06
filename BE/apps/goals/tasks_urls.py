670a5e2ffdca8f191502cf54105ad2f705c15b0b
Tuan Kiet <kiet.truong12082004@hcmut.edu.vn>
2026-04-06 22:06:24 +0700

    remove unused library and add productivity data for dashboard

--- BE/apps/goals/tasks_urls.py
+++ BE/apps/goals/tasks_urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TaskDetailView, TaskListView
+from .views import TaskDetailView, TaskListView, TaskProductivityView
 
 urlpatterns = [
     # Task endpoints
     path('', TaskListView.as_view(), name='task-list'),  # List all tasks
-    path('/<uuid:pk>', TaskDetailView.as_view(), name='task-detail')  # Get, update, delete a specific task
+    path('/<uuid:pk>', TaskDetailView.as_view(), name='task-detail'),  # Get, update, delete a specific task
+    path('/productivity', TaskProductivityView.as_view(), name='task-productivity'),
 ]
