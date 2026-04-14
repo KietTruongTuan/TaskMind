c46c8ede7aafd907537318bdbaca7d9ce6d7a87c
KoderuNoKo <4trung9kien123@gmail.com>
2026-04-14 17:29:44 +0700

    fix: update url of knowledge base upload endpoint

--- BE/apps/knowledge_base/urls.py
+++ BE/apps/knowledge_base/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import DocumentUploadProcessView
 
 urlpatterns = [
-    path('/documents/upload/process', DocumentUploadProcessView.as_view(), name='document-upload-process'),
+    path('/upload', DocumentUploadProcessView.as_view(), name='document-upload-process'),
 ]
