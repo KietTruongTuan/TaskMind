13445bcce659d3d44c7cfd40fcdd34f35cfaea6e
KoderuNoKo <4trung9kien123@gmail.com>
2026-05-02 17:49:56 +0700

    feat: add delete document from knowledge base

--- BE/apps/knowledge_base/urls.py
+++ BE/apps/knowledge_base/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import DocumentUploadProcessView
+from .views import DocumentUploadProcessView, DocumentDetailView
 
 urlpatterns = [
     path('/documents', DocumentUploadProcessView.as_view(), name='document-upload-process'),
+    path('/documents/<int:pk>/', DocumentDetailView.as_view(), name='document-detail')
 ]
